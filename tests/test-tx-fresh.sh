d65bba3912a9118fcdc4b6bec96205c833a4ea90
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-04-09 22:50:00 -0400

    print client.toml

--- tests/test-tx-fresh.sh
+++ tests/test-tx-fresh.sh
@@ -16,6 +16,7 @@ check_code()
 
 echo $HOME_1
 cat $HOME_1/config/config.toml
+cat $HOME_1/config/client.toml
 echo "Sending funds with tx bank send..."
 TXHASH=$($CHAIN_BINARY tx bank send $WALLET_1 $WALLET_2 100003000$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --fees 1000$DENOM --chain-id $CHAIN_ID -y -o json -b block | jq '.txhash' | tr -d '"')
 check_code $TXHASH
