ed2d28dffede1a6de6ab14c09c52bdc1f07c8606
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-04-09 21:36:14 -0400

    tx test

---
+++ tests/test-tx-fresh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Test transactions with a fresh state.
+
+
+check_code()
+{
+  txhash=$1
+  code=$($CHAIN_BINARY q tx $txhash -o json | jq '.code')
+  if [ $code -eq 0 ]
+  then
+    return 0
+  else
+    return 1
+  fi
+}
+
+echo "Sending funds with tx bank send..."
+TXHASH=$($CHAIN_BINARY tx bank send $WALLET_1 $WALLET_2 100003000$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --fees 1000$DENOM --chain-id $CHAIN_ID -y -o json -b block | jq '.txhash' | tr -d '"')
+check_code $TXHASH
