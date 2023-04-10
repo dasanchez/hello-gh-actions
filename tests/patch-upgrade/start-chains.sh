f39074488b19c388d4901cbbdea6125622ae0a89
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-04-09 21:29:53 -0400

    service names

--- tests/patch-upgrade/start-chains.sh
+++ tests/patch-upgrade/start-chains.sh
@@ -100,44 +100,44 @@ cp $HOME_1/config/genesis.json $HOME_2/config/genesis.json
 
 echo "Setting up services..."
 
-rm /etc/systemd/system/$SERVICE_1
-touch /etc/systemd/system/$SERVICE_1
-echo "[Unit]"                               | sudo tee /etc/systemd/system/$SERVICE_1
-echo "Description=Gaia service"       | sudo tee /etc/systemd/system/$SERVICE_1 -a
-echo "After=network-online.target"          | sudo tee /etc/systemd/system/$SERVICE_1 -a
-echo ""                                     | sudo tee /etc/systemd/system/$SERVICE_1 -a
-echo "[Service]"                            | sudo tee /etc/systemd/system/$SERVICE_1 -a
-echo "User=$USER"                            | sudo tee /etc/systemd/system/$SERVICE_1 -a
-echo "ExecStart=$HOME/go/bin/$CHAIN_BINARY start --x-crisis-skip-assert-invariants --home $HOME_1" | sudo tee /etc/systemd/system/$SERVICE_1 -a
-echo "Restart=no"                       | sudo tee /etc/systemd/system/$SERVICE_1 -a
-echo "LimitNOFILE=4096"                     | sudo tee /etc/systemd/system/$SERVICE_1 -a
-echo ""                                     | sudo tee /etc/systemd/system/$SERVICE_1 -a
-echo "[Install]"                            | sudo tee /etc/systemd/system/$SERVICE_1 -a
-echo "WantedBy=multi-user.target"           | sudo tee /etc/systemd/system/$SERVICE_1 -a
+rm /etc/systemd/system/$PROVIDER_SERVICE_1
+touch /etc/systemd/system/$PROVIDER_SERVICE_1
+echo "[Unit]"                               | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_1
+echo "Description=Gaia service"       | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_1 -a
+echo "After=network-online.target"          | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_1 -a
+echo ""                                     | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_1 -a
+echo "[Service]"                            | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_1 -a
+echo "User=$USER"                            | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_1 -a
+echo "ExecStart=$HOME/go/bin/$CHAIN_BINARY start --x-crisis-skip-assert-invariants --home $HOME_1" | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_1 -a
+echo "Restart=no"                       | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_1 -a
+echo "LimitNOFILE=4096"                     | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_1 -a
+echo ""                                     | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_1 -a
+echo "[Install]"                            | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_1 -a
+echo "WantedBy=multi-user.target"           | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_1 -a
 
-rm /etc/systemd/system/$SERVICE_2
-touch /etc/systemd/system/$SERVICE_2
-echo "[Unit]"                               | sudo tee /etc/systemd/system/$SERVICE_2
-echo "Description=Gaia service"       | sudo tee /etc/systemd/system/$SERVICE_2 -a
-echo "After=network-online.target"          | sudo tee /etc/systemd/system/$SERVICE_2 -a
-echo ""                                     | sudo tee /etc/systemd/system/$SERVICE_2 -a
-echo "[Service]"                            | sudo tee /etc/systemd/system/$SERVICE_2 -a
-echo "User=$USER"                            | sudo tee /etc/systemd/system/$SERVICE_2 -a
-echo "ExecStart=$HOME/go/bin/$CHAIN_BINARY start --x-crisis-skip-assert-invariants --home $HOME_2" | sudo tee /etc/systemd/system/$SERVICE_2 -a
-echo "Restart=no"                       | sudo tee /etc/systemd/system/$SERVICE_2 -a
-echo "LimitNOFILE=4096"                     | sudo tee /etc/systemd/system/$SERVICE_2 -a
-echo ""                                     | sudo tee /etc/systemd/system/$SERVICE_2 -a
-echo "[Install]"                            | sudo tee /etc/systemd/system/$SERVICE_2 -a
-echo "WantedBy=multi-user.target"           | sudo tee /etc/systemd/system/$SERVICE_2 -a
+rm /etc/systemd/system/$PROVIDER_SERVICE_2
+touch /etc/systemd/system/$PROVIDER_SERVICE_2
+echo "[Unit]"                               | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_2
+echo "Description=Gaia service"       | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_2 -a
+echo "After=network-online.target"          | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_2 -a
+echo ""                                     | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_2 -a
+echo "[Service]"                            | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_2 -a
+echo "User=$USER"                            | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_2 -a
+echo "ExecStart=$HOME/go/bin/$CHAIN_BINARY start --x-crisis-skip-assert-invariants --home $HOME_2" | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_2 -a
+echo "Restart=no"                       | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_2 -a
+echo "LimitNOFILE=4096"                     | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_2 -a
+echo ""                                     | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_2 -a
+echo "[Install]"                            | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_2 -a
+echo "WantedBy=multi-user.target"           | sudo tee /etc/systemd/system/$PROVIDER_SERVICE_2 -a
 
 sudo systemctl daemon-reload
-sudo systemctl enable $SERVICE_1 --now
-sudo systemctl enable $SERVICE_2 --now
+sudo systemctl enable $PROVIDER_SERVICE_1 --now
+sudo systemctl enable $PROVIDER_SERVICE_2 --now
 
 # echo "***********************"
 # echo "To see the Gaia log enter:"
-# echo "journalctl -fu $SERVICE_1"
-# echo "journalctl -fu $SERVICE_2"
+# echo "journalctl -fu $PROVIDER_SERVICE_1"
+# echo "journalctl -fu $PROVIDER_SERVICE_2"
 # echo "***********************"
 
 echo "Waiting for chains to start and endpoints to become available..."
