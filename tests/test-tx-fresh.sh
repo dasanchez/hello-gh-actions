776890552449b0542f292dad3bed6016ab7b0096
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-04-09 23:22:58 -0400

    home flag

--- tests/test-tx-fresh.sh
+++ tests/test-tx-fresh.sh
@@ -5,7 +5,7 @@
 check_code()
 {
   txhash=$1
-  code=$($CHAIN_BINARY q tx $txhash -o json | jq '.code')
+  code=$($CHAIN_BINARY q tx $txhash -o json --home $HOME_1 | jq '.code')
   if [ $code -eq 0 ]
   then
     return 0
@@ -15,7 +15,6 @@ check_code()
 }
 
 $CHAIN_BINARY q bank balances $WALLET_1 --home $HOME_1
-$CHAIN_BINARY tx bank send $WALLET_1 $WALLET_2 100003000$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas auto --fees 1000$DENOM --chain-id $CHAIN_ID -y -o json -b block
-# echo "Sending funds with tx bank send..."
-# TXHASH=$($CHAIN_BINARY tx bank send $WALLET_1 $WALLET_2 100003000$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --fees 1000$DENOM --chain-id $CHAIN_ID -y -o json -b block | jq '.txhash' | tr -d '"')
-# check_code $TXHASH
+echo "Sending funds with tx bank send..."
+TXHASH=$($CHAIN_BINARY tx bank send $WALLET_1 $WALLET_2 100003000$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --fees 1000$DENOM --chain-id $CHAIN_ID -y -o json -b block | jq '.txhash' | tr -d '"')
+check_code $TXHASH
